9a62152f3a6aef005831533401337f13eca911d9
savedev <savedev@example.com>
2021-11-07 17:52:31 -0500

    initial commit

---
+++ spankbangTitle.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Reddit Gif Sound
+// @namespace    https://github.com/VoltronicAcid/
+// @version      0.0.1
+// @description  Remove "Watch" from the title of videos on spankbang.com
+// @author       VoltronicAcid
+// @match        https://spankbang.com/*/video/*
+// @match        https://*.spankbang.com/*/video/*
+// ==/UserScript==
