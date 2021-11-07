1d75d2bb42cc585c8ac1dbf0d5f5c5285649d107
savedev <savedev@example.com>
2021-11-07 18:58:44 -0500

    no frames

--- spankbangTitle.user.js
+++ spankbangTitle.user.js
@@ -2,11 +2,12 @@
 // @name         SpankBang Title
 // @namespace    https://github.com/VoltronicAcid/
 // @downloadURL  https://github.com/VoltronicAcid/spankbangTitle/raw/master/spankbangTitle.user.js
-// @version      0.0.2
+// @version      1.0.0
 // @description  Remove "Watch" from the title of videos on spankbang.com
 // @author       VoltronicAcid
 // @match        https://spankbang.com/*/video/*
 // @match        https://*.spankbang.com/*/video/*
+// @noframes
 // ==/UserScript==
 
 document.title = document.title.substring(6);
